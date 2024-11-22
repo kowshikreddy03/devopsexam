ce225d8fae04c8e3f23c302169db4dd5fab1437d
Kowshik Reddy <140056704+kowshikreddy03@users.noreply.github.com>
2024-11-22 10:53:26 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,19 +1,18 @@
 public class Prime {
     public static void main(String[] args) {
-        int number = 29; // Example number
+        int num = 29;
         boolean isPrime = true;
 
-        for (int i = 2; i <= number / 2; ++i) {
-            if (number % i == 0) {
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
                 isPrime = false;
                 break;
             }
         }
 
-        if (isPrime) {
-            System.out.println(number + " is a prime number.");
-        } else {
-            System.out.println(number + " is not a prime number.");
-        }
+        if (isPrime)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
 }
